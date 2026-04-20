b3e50a1dca2c7d277a83711561714df35c9ac5e1
Abdiel Lopez <48071553+PaperPrototype@users.noreply.github.com>
2026-04-20 01:55:25 -0400

    Pushed everything idc

---
+++ Assets/PrototypeDemo/PlayerInput.cs
@@ -0,0 +1,17 @@
+using Prowl.Runtime;
+
+public class PlayerInput : MonoBehaviour
+{
+
+    public float MetersPerSecond = 1;
+
+
+    public override void Start()
+    {
+        
+    }
+
+    public override void Update()
+    {
+    }
+}
